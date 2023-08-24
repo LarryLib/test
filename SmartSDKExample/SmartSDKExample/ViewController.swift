ae41b8e5a651b3822f066f6538b6daf3df470724
LarryMacPro <lxd_super_main@126.com>
2023-08-24 17:02:23 +0800

    wip

---
+++ SmartSDKExample/SmartSDKExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//   ViewController.swift
+//   SmartSDKExample
+//
+//   Created by LMP
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
